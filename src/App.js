b85ea531231bf2872567839e44578f6bc3429f83
KyleMoore <kylemoore-@hotmail.com>
2023-02-01 14:50:39 -0500

    fixed errors and react issues

--- src/App.js
+++ src/App.js
@@ -1,44 +1,33 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import firebase from './firebase';
-import Form from './components/Form';
+import firebase from "./firebase";
 
-import { getDatabase, onValue, push, ref, remove } from 'firebase/database'
+import { getDatabase, onValue, push, ref, remove } from "firebase/database";
 
-import './App.css';
-
-
-
-
+import "./App.css";
 
 function App() {
-
-  const [books, setBooks] = useState([])
-  const [userInput, setUserInput] = useState("")
-
-
+  const [books, setBooks] = useState([]);
+  const [userInput, setUserInput] = useState("");
 
   // Inputting books on the screen
 
   const handleInputChange = (event) => {
     setUserInput(event.target.value);
-  }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    const database = getDatabase(firebase)
+    const database = getDatabase(firebase);
     const dbRef = ref(database);
 
     push(dbRef, userInput);
 
-    setUserInput("")
-  }
+    setUserInput("");
+  };
 
   // Inputting books on screen end
-
-
-
 
   // Removing books start
 
@@ -46,15 +35,10 @@ function App() {
     const database = getDatabase(firebase);
     const dbRef = ref(database, `${bookId}`);
 
-  
     remove(dbRef);
-  }
+  };
 
   // Removing books end
-
-
-
-
 
   // For getting data from Firebase
   useEffect(() => {
@@ -70,26 +54,23 @@ function App() {
       for (let key in data) {
         newState.push({
           key: key,
-          name: data[key]
-        })
+          name: data[key],
+        });
       }
       setBooks(newState);
-    })
-  }, [])
+    });
+  }, []);
   // End of getting data from Firebase
-
 
   return (
     <div className="App">
-
       <h1>My Bookshelf!</h1>
-
 
       <form action="submit">
         <label htmlFor="newBook">Add a book to your bookshelf: </label>
         <input
           onChange={handleInputChange}
-          type='text'
+          type="text"
           id="newBook"
           placeholder="Book Title"
           value={userInput}
@@ -97,20 +78,22 @@ function App() {
         <button onClick={handleSubmit}>Add Book</button>
       </form>
 
-
       <ul>
         {books.map((book) => {
           return (
             <li key={book.key}>
               <p>📚 {book.name}</p>
-              <button onClick={() => { handleRemoveBook(book.key) }}>
+              <button
+                onClick={() => {
+                  handleRemoveBook(book.key);
+                }}
+              >
                 Remove Book
               </button>
             </li>
-          )
+          );
         })}
       </ul>
-
     </div>
   );
 }
